0ef42ac8cc6eb89f0579c90cf6fc16efa1c4aa8a
urus.urusik1337 <urus.urusik1337@gmail.com>
2023-11-15 11:41:24 +0700

    small changes, add Moq framework to sln

--- UnitTests/HospitalModelTests.cs
+++ UnitTests/HospitalModelTests.cs
@@ -1,4 +1,6 @@
 using Model.Data;
+using Model.Data.Interfaces;
+using Model.Data.Repositories;
 using Model.Model;
 using System;
 using Xunit;
@@ -11,12 +13,20 @@ namespace UnitTests
         private HospitalModel _hospitalModel;
         public HospitalModelTests()
         {
-            _repository = new HospitalRepository();
+            HospitalContext context = new HospitalContext();
+            IAppointmentRepo appointmentRepo = new AppointmentRepo(context);
+            IAppointmentTimeRepo appointmentTimeRepo = new AppointmentTimeRepo(context);
+            IDoctorRepo doctorRepo = new DoctorRepo(context);
+            IPatientRepo patientRepo = new PatientRepo(context);
+            ITypeDoctorRepo typeDoctorRepo = new TypeDoctorRepo(context);
+
+            _repository = new HospitalRepository(appointmentRepo, appointmentTimeRepo,
+                                                            doctorRepo, patientRepo, typeDoctorRepo);
             _hospitalModel = new HospitalModel(_repository);
         }
          
         [Fact]
-        public void CallCreateAppointmentTest()
+        public void CreateAppointment()
         {
             // Arrange
             PatientModel patientModel = new PatientModel(1, "Peter", "Griffin"); 
@@ -30,16 +40,49 @@ namespace UnitTests
             // Assert
             Assert.Equal("OK", result);
 
+        }
+
+        public void CallCreateAppointmentTest_PatientNull()
+        {
+            // Arrange
+            DoctorModel doctorModel = new DoctorModel(1, "Иван", "Сидоров", new TypeDoctorModel(1, "Диетолог"));
+            AppointmentTimeModel appointmentTimeModel = new AppointmentTimeModel(1, new TimeSpan(10, 30, 00),
+                new TimeSpan(11, 00, 00));
+
             // Act
-            result = _hospitalModel.CallCreateAppointment(null, doctorModel, appointmentTimeModel);
+            string result = _hospitalModel.CallCreateAppointment(null, doctorModel, appointmentTimeModel);
 
             // Assert
             Assert.Equal("Error", result);
-
         }
+
+
 
         [Fact]
         public void CreatePatientTest()
+        {
+            // Arrange
+            Random random = new Random();
+
+            string name = "Bart" + random.Next();
+            string surname = "Griffin" + random.Next();
+
+            // Act
+            PatientModel? patient = _hospitalModel.CreatePatient(name, surname);
+
+            // Assert
+            Assert.NotNull(patient);
+
+            // Act
+            patient = _hospitalModel.CreatePatient(name, surname);
+
+            // Assert
+            Assert.Null(patient);
+        }
+
+
+        [Fact]
+        public void CreatePatientTesta()
         {
             // Arrange
             Random random = new Random();
