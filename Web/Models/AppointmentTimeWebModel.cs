18c52d76cd695fdede8b2459615b7581939f583f
urus.urusik1337 <urus.urusik1337@gmail.com>
2023-11-15 09:34:08 +0700

    small changes

--- Web/Models/AppointmentTimeWebModel.cs
+++ Web/Models/AppointmentTimeWebModel.cs
@@ -2,5 +2,8 @@
 {
     public class AppointmentTimeWebModel
     {
+        public int Id { get; set; }
+        public TimeSpan StartTime { get; set; }
+        public TimeSpan EndTime { get; set; }
     }
 }
