cbf010c9b783bd43cad4650e7e52d68b42f3423b
Alex <urus.urusik1337@gmail.com>
2023-12-27 00:32:58 +0700

    added class Mapper

---
+++ Mappers/Mapper.cs
@@ -0,0 +1,45 @@
+﻿using Model.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mappers
+{
+    public static class Mapper
+    {
+        private static DoctorModelToStrMapper _doctorModelToStrMapper = new DoctorModelToStrMapper();
+        private static AppointmentTimeModelToStrMapper _appointmentTimeModelToStrMapper = new AppointmentTimeModelToStrMapper();
+        private static TypeDoctorModelToStrMapper _typeDoctorModelToStrMapper = new TypeDoctorModelToStrMapper();
+
+        public static string ModelToString(object model)
+        {
+            if (model is DoctorModel doctor)
+            {
+                return _doctorModelToStrMapper.ModelToString(doctor);
+            }
+            if (model is AppointmentTimeModel appointmentTime)
+            {
+                return _appointmentTimeModelToStrMapper.ModelToString(appointmentTime);
+            }
+            if (model is TypeDoctorModel typeDoctor)
+            {
+                return _typeDoctorModelToStrMapper.ModelToString(typeDoctor);
+            }
+            return "Unknown";
+        }
+
+        public static ICollection<string> ConvertModelListToString<Model>(ICollection<Model> models) where Model : class
+        {
+            List<string> listDoctorModelStrings = new List<string>();
+
+            foreach (var model in models)
+            {
+                listDoctorModelStrings.Add(ModelToString(model));
+            }
+
+            return listDoctorModelStrings;
+        }
+    }
+}
